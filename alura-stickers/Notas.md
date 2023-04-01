a6918dd886c890458ac935973ed35f302621e914
Valdir Torres <valdyrtorres@yahoo.com.br>
2023-03-31 22:01:59 -0300

    3o. dia

--- alura-stickers/Notas.md
+++ alura-stickers/Notas.md
@@ -64,3 +64,27 @@ Nota:
 [6 < rating <= 8] => 4 estrelas
 [8 < rating <= 10] => 5 estrelas
 
+Aula 3
+https://api.nasa.gov/
+You are receiving this email to confirm the creation of an API key. If you did not request this, please disregard this email. Your API key for valdyrtorres@yahoo.com.br is:
+
+A3lnceIBNfWY5pTiNGDknbT0drCbVCYbI382XKRn
+
+DEMO_KEY Rate Limits
+In documentation examples, the special DEMO_KEY api key is used. This API key can be used for initially exploring APIs prior to signing up, but it has much lower rate limits, so you’re encouraged to signup for your own API key if you plan to use the API (signup is quick and easy). The rate limits for the DEMO_KEY are:
+
+Hourly Limit: 30 requests per IP address per hour
+Daily Limit: 50 requests per IP address per day
+How Do I See My Current Usage?
+Your can check your current rate limit and usage details by inspecting the X-RateLimit-Limit and X-RateLimit-Remaining HTTP headers that are returned on every API response. For example, if an API has the default hourly limit of 1,000 request, after making 2 requests, you will receive this HTTP header in the response of the second request:
+
+X-RateLimit-Remaining: 998
+
+The hourly counters for your API key reset on a rolling basis.
+
+Example: If you made 500 requests at 10:15AM and 500 requests at 10:25AM, your API key would become temporarily blocked. This temporary block of your API key would cease at 11:15AM, at which point you could make 500 requests. At 11:25AM, you could then make another 500 requests.
+
+Anyone can register for an api.nasa.gov key, which can be used to access data across federal agencies.
+
+LINK: https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14
+
--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,24 +1,26 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
+        // Nasa api
+        String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
+        ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
+
         // fazer uma conexão http e buscar os top 250 filmes
         //String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
 
         // TopMovies
-        //String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        // String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        // ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
+
         // Most Popular Movies
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
+        //String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
 
         // endpoint que retorna as melhores séries
         //String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopTVs.json";
@@ -26,36 +28,27 @@ public class App {
         // as séries mais populares
         //String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularTVs.json";
 
-        URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
-        //System.out.println(body);
-
-        // extrair só os dados que interessam (titulo, poster, classificação)
-        var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
-        //System.out.println(listaDeFilmes.size());
-        //System.out.println(listaDeFilmes.get(0));
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url);
 
         var diretorio = new File("figurinhas/");
         diretorio.mkdir();
 
         // exibir e manipular os dados
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
+
         var geradora = new GeradoraDeFigurinhas();
-        for (Map<String, String> filme : listaDeFilmes) {
+        for (Conteudo conteudo : conteudos) {
 
-            String urlImagem = filme.get("image");
-            String titulo = filme.get("title");
+            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+            String nomeArquivo = "figurinhas/" + conteudo.getTitulo() + ".png";
 
-            InputStream inputStream = new URL(urlImagem).openStream();
-            String nomeArquivo = "figurinhas/" + titulo + ".png";
+            System.out.println("Título: " + conteudo.getTitulo() );
+            System.out.println("Poster: " + conteudo.getUrlImagem());
+            // String nota = filme.get("imDbRating");
+            // double classificacao = Double.parseDouble(nota);
 
-            System.out.println("Título: " + titulo);
-            System.out.println("Poster: " + urlImagem);
-            String nota = filme.get("imDbRating");
-            double classificacao = Double.parseDouble(nota);
+            double classificacao = 8.9;
 
             String textoFigurinha;
             InputStream imagemValdyr;
@@ -69,21 +62,21 @@ public class App {
 
             geradora.criar(inputStream, nomeArquivo, textoFigurinha, imagemValdyr);
 
-            System.out.println("\u001b[37;1m\u001b[44;1mClassificação: " + nota + "\u001b[m");
+            // System.out.println("\u001b[37;1m\u001b[44;1mClassificação: " + nota + "\u001b[m");
             //System.out.println("\uD83D\uDC99 💙️");
-            int rating = 1;
-            double numberRating = Double.parseDouble(nota);
-            if (numberRating > 2d && numberRating <= 4d)
-                rating = 2;
-            else if (numberRating > 4d && numberRating <= 6d)
-                rating = 3;
-            else if (numberRating > 6d && numberRating <= 8d)
-                rating = 4;      
-            else if (numberRating > 8d && numberRating <= 10d)
-                rating = 5;                     
-            for(int i = 0; i < rating; i++)
-                System.out.print("\u2B50");
-            System.out.println();
+            // int rating = 1;
+            // double numberRating = Double.parseDouble(nota);
+            // if (numberRating > 2d && numberRating <= 4d)
+            //     rating = 2;
+            // else if (numberRating > 4d && numberRating <= 6d)
+            //     rating = 3;
+            // else if (numberRating > 6d && numberRating <= 8d)
+            //     rating = 4;      
+            // else if (numberRating > 8d && numberRating <= 10d)
+            //     rating = 5;                     
+            // for(int i = 0; i < rating; i++)
+            //     System.out.print("\u2B50");
+            // System.out.println();
         }
     }
 }
