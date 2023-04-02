3911c9c926e27bbaa03baa2de4d6429e4f7dac73
Valdir Torres <valdyrtorres@yahoo.com.br>
2023-04-02 18:41:00 -0300

    desafios 2 e 3 - ordenar e code 201

--- linguagens-api/src/main/java/br/com/alura/linguagens/api/LinguagemController.java
+++ linguagens-api/src/main/java/br/com/alura/linguagens/api/LinguagemController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,14 +22,14 @@ public class LinguagemController {
 
     @GetMapping("/linguagens")
     public List<Linguagem> obterLinguagens() {
-        List<Linguagem> linguagens = repositorio.findAll();
+        List<Linguagem> linguagens = repositorio.findByOrderByRanking();
         return linguagens;
     }
 
     @PostMapping("/linguagens")
-    public Linguagem cadastrarLinguagem(@RequestBody Linguagem linguagem) {
+    public ResponseEntity<Linguagem> cadastrarLinguagem(@RequestBody Linguagem linguagem) {
         Linguagem linguagemSalva = repositorio.save(linguagem);
-        return linguagemSalva;
+        return new ResponseEntity<>(linguagemSalva, HttpStatus.CREATED);
     }
 
     @GetMapping("/linguagens/{id}")
